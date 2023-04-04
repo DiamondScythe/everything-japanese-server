84282bc1eaa306f974d0024601fe3e2ad613b680
DiamondScythe <nhutoan399@gmail.com>
2023-04-04 16:03:31 +0700

    added authentication system, added mongodb server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('../routes/authRoutes');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+
+//database connection
+const dbURI = 'mongodb://localhost:27017/bookstore';
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+//routes
 app.get("/test", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(1337, () => {  
-  console.log('Server listening on port 1337');
-});
+app.use(authRoutes);
